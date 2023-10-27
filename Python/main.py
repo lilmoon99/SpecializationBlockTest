aacd229db0e7d95bdcf7bed8be31a22d5439f20b
Aynur Shamsullin <lilmoon99@ya.ru>
2023-10-27 18:09:49 +0400

    task 1 completed

--- Python/main.py
+++ Python/main.py
@@ -1,23 +1,73 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import json
+from notebook import *
 from note import Note
-from notebook import Notebook
-from datetime import datetime
 
-# json_data = json.dumps(data, sort_keys=True, indent=4)
-# print(json_data)
-
-# with open('data.json', 'w') as file:
-#     json.dump(data, file)
-note1 = Note("Header", "Body", )
-# notebook.show_notes()
-note2 = Note("Header2", "Body2")
 notebook = Notebook()
-# notebook.add_note(note1)
-# notebook.add_note(note2)
-# notebook.show_notes()
-notebook.load_notebook()
-notebook.show_notes()
+isContinue = True
+
+
+def create_note():
+    print("Note creating module activated...")
+    note = Note(input("Note header: "), input("Note body: "), generate_id())
+    notebook.add_note(note)
+    print("Note created.")
+    save_notebook()
+
+
+def show_all():
+    for x in notebook.list:
+        print(x)
+
+
+def edit_note():
+    print("Note editor activated...")
+    note_edit_id = int(input("Enter note id to be edited: "))
+    notebook.show_note(note_edit_id)
+    notebook.edit_note(note_edit_id)
+    save_notebook()
+
+
+def delete_note():
+    print("Note deleting mode activated...")
+    try:
+        note_delete_id = int(input("Enter note id to be deleted: "))
+        notebook.remove_note(note_delete_id)
+    except ValueError:
+        print("Oops!  That was no valid number.")
+
+
+def save_notebook():
+    notebook.save_notebook()
+
+
+def load_notebook():
+    notebook.load_notebook()
+
+
+while isContinue:
+    print("""
+    1 - create note
+    2 - show all
+    3 - edit note
+    4 - delete note
+    5 - load notes
+    6 - exit program
+    """)
+
+    command = input("Your choice: ")
+
+    match command:
+        case "1":
+            create_note()
+        case "2":
+            show_all()
+        case "3":
+            edit_note()
+        case "4":
+            delete_note()
+        case "5":
+            load_notebook()
+        case "6":
+            print("Good bye")
+            isContinue = False
+        case _:
+            print("You entered wrong command!")
--- Python/note.py
+++ Python/note.py
@@ -5,15 +5,17 @@ from typing import Any
 
 class Note:
 
-    def __init__(self, header, body):
+    def __init__(self, header, body, note_id):
         self.header = header
         self.body = body
         self.datetime = str(datetime.now())
-
-
+        self.note_id = int(note_id)
 
     def __str__(self):
-        return f"Header: {self.header}\nBody: {self.body}\nCreate date: {self.datetime}"
+        return (f"Header: {self.header}\n"
+                f"Body: {self.body}\n"
+                f"Create date: {self.datetime}\n"
+                f"ID: {self.note_id}")
 
     def __setattr__(self, __name: str, __value: Any) -> None:
         super().__setattr__(__name, __value)
